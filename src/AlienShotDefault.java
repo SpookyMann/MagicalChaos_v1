5f110b0439f0a60f23b5fce8607be8c34ec8a88c
Kaia <75691544+Omnificent101@users.noreply.github.com>
2020-12-14 10:20:47 -0800

    Update AlienShotDefault.java

--- src/AlienShotDefault.java
+++ src/AlienShotDefault.java
@@ -6,9 +6,9 @@
 //shooting class
 public class AlienShotDefault extends Entity {
 
-  private double moveSpeed = 300; // horizontal speed shot moves
-  private boolean used = false; // true if shot hits something
-  private Game game; // the game in which the ship exists
+	private double moveSpeed = 300; // horizontal speed shot moves
+	private boolean used = false; // true if shot hits something
+	private Game game; // the game in which the ship exists
 
   /* construct the shot
    * input: game - the game in which the shot is being created
@@ -16,25 +16,25 @@ public class AlienShotDefault extends Entity {
    *              the sprite for the shot
    *        x, y - initial location of shot
    */
-  public AlienShotDefault(Game g, String r, int newX, int newY) {
-    super(r, newX, newY);  // calls the constructor in Entity
-    game = g;
-    dx = -moveSpeed;
-  } // constructor
+	public AlienShotDefault(Game g, String r, int newX, int newY) {
+		super(r, newX, newY);  // calls the constructor in Entity
+		game = g;
+		dx = -moveSpeed;
+	} // constructor
 
   /* move
    * input: delta - time elapsed since last move (ms)
    * purpose: move shot
    */
-  public void move (long delta){
-    super.move(delta);  // calls the move method in Entity
+	public void move (long delta){
+		super.move(delta);  // calls the move method in Entity
 
     // if shot moves off top of screen, remove it from entity list
-    if (x < -20) {
-      game.removeEntity(this);
-    } // if
+		if (x < -20) {
+			game.removeEntity(this);
+		} // if
 
-  } // move
+	} // move
 
 
   /* collidedWith
@@ -42,23 +42,23 @@ public class AlienShotDefault extends Entity {
    * purpose: notification that the shot has collided
    *          with something
    */
-   public void collidedWith(Entity other) {
+	public void collidedWith(Entity other) {
      // prevents double kills
-     if (used) {
-       return;
-     } // if
+		if (used) {
+			return;
+		} // if
      
      // if it has hit an alien, kill it!
-     if (other instanceof ShipEntity) {
-       // remove affect entities from the Entity list
-       game.removeEntity(this);
-       game.removeEntity(other);
+		if (other instanceof ShipEntity) {
+			// remove affect entities from the Entity list
+			game.removeEntity(this);
+			game.removeEntity(other);
        
        // notify the game that the alien is dead
-       game.notifyDeath();
-       used = true;
-     } // if
+			game.notifyDeath();
+			used = true;
+		} // if
 
    } // collidedWith
 
-} // ShipEntity class
+} //AlienShotDefaultclass
